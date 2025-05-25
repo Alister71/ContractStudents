8a9adb008c62399e5e32af1c6df3c4562f2c0645
Serhii Kushnerenko <serhii_kushnerenko@epam.com>
2025-05-25 15:46:28 +0300

    Initial commit

---
+++ src/test/java/com/students/contractstudents/ContractStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.students.contractstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractStudentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
